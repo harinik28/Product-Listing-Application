1a5ddd6ac57514a42e0fa0bacb7d2db5e1700e5a
Harini Dharshini <Harini Dharshini@DESKTOP-RT1P30B>
2024-12-29 12:20:02 +0530

    Add project files.

---
+++ CRUDOperationsMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUDOperationsMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
